d26709e91b46ee47287d4d4eb068e99273c4600f
mateusguidotti <guidottimateus1@gmail.com>
2026-04-06 16:31:04 -0300

    adiciona menu hambúrguer responsivo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,18 @@
 import './Navbar.css'
 import logo from './assets/react.svg'
+import { useState } from 'react'
 
-function Navbar(){
-     return(
+function Navbar() {
+     const [menuAberto, setMenuAberto] = useState(false);
+
+     const alterarMenu = () => {
+          setMenuAberto(!menuAberto);
+     }
+
+
+     return (
           <div className="nav">
-               <img src={logo} alt="Minha logo Pica" className='logo'/>
+               <img src={logo} alt="Minha logo Pica" className='logo' />
 
                <ul className='nav-list'>
                     <li>
@@ -20,17 +28,41 @@ function Navbar(){
                          <a href="#">Contato</a>
                     </li>
                </ul>
-               
+
                <div className="cta-section">
-                    <img src={logo} alt="logo" className='cta-image'/>
+                    <img src={logo} alt="logo" className='cta-image' />
                     <button className='cta-button'>Compre Agora</button>
 
                </div>
 
-               <div className="burguer-menu">
+               <div className="burguer-menu" onClick={alterarMenu}>
                     <span></span>
                     <span></span>
                     <span></span>
+               </div>
+
+               <div className={`modal-mobile ${menuAberto ? "aberto" : ""}`}>
+                    <ul>
+                         <li>
+                              <a href="#">Início</a>
+                         </li>
+                         <li>
+                              <a href="#">Cardápio</a>
+                         </li>
+                         <li>
+                              <a href="#">Sobre</a>
+                         </li>
+                         <li>
+                              <a href="#">Contato</a>
+                         </li>
+                    </ul>
+
+                    <span></span>
+
+                    <div className="cta-section-mobile">
+                         <img src={logo} alt="logo" className='cta-image' />
+                         <button className='cta-button'>Compre Agora</button>
+                    </div>
                </div>
           </div>
      )
